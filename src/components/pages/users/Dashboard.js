c5aae5f7d3dca1cea49aebd80a5496abd61edcfa
vishkhi123 <vishalkhiratkar@gmail.com>
2024-01-29 15:14:09 +0530

    Private Route Implemented

--- src/components/pages/users/Dashboard.js
+++ src/components/pages/users/Dashboard.js
@@ -1,12 +1,48 @@
-import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import UserContext from "../../context/user.context";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 const Dashboard=()=>{
 
+    const userContext=useContext(UserContext);
+
+    //Private Dashboard
+    const dashboardView=()=>{
+        return(
+            <div>
+              This Is User Dashboard
+              <Outlet/>
+            </div>
+
+        )
+    }
+//not LoggedIn View!!!
+
+const notLoggedInView=()=>{
+    return(
+        <Container >
+            <Row>
+                <Col md={{
+                    span:8,
+                    offset:2
+                }}>
+                    <Card className="border-0 shadow mt-3">
+                        <Card.Body className="text-center">
+                        <h3>You Are Not Logged In !!!!</h3>
+                        <p>Please do Login to view the Page !!!</p>
+                        <Button as={NavLink} to={"/login"} variant="success" >Login Now </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+
 return(
-    <div>
-        This Is User Dashboard
-        <Outlet/>
-    </div>
+    (userContext.isLogin) ? dashboardView() : notLoggedInView()
 )
 
 
